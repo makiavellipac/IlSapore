0572d3b78b6d2cf67880828ad4fdd003c238b605
MakiavelliPac <ridesburak@gmail.com>
2020-03-04 14:14:54 -0600

    Se agrego la card de compra y la funcionalidad de acordeon y escoger productos al momento de comprar

---
+++ ilSaporeClient/src/components/CardCompra/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Box, Text, Image} from '@chakra-ui/core'
+
+function CardCompra({ nombre, ingredientes,precio,img}) {
+  return (
+    <Box maxW="sm" borderWidth="1px" rounded="lg" overflow="hidden">
+      <Image src={img} alt={nombre} />
+      <Box
+          mt="1"
+          fontWeight="semibold"
+          as="h4"
+          lineHeight="tight"
+          isTruncated
+          color="Yellow"
+        >
+          {nombre}
+        </Box>
+
+      <Box p="6">
+        <Box d="flex" alignItems="baseline">
+          <Box
+            color="gray.500"
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="xs"
+            textTransform="uppercase"
+            ml="2"
+          >
+          {ingredientes.map(ingrediente=>
+            <>
+            <Text color="white">{ingrediente} &bull;</Text>
+            </>
+          )}
+          </Box>
+        </Box>
+        <Box>
+          <Text color="white">${precio}/Mx</Text>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default CardCompra
--- ilSaporeClient/src/pages/Pedido/index.js
+++ ilSaporeClient/src/pages/Pedido/index.js
@@ -1,7 +1,10 @@
 import React,{ useEffect,useContext }  from 'react'
 import "./index.css"
 import { MyContext } from '../../context'
-
+import { Grid, Flex } from '@chakra-ui/core'
+import Card from "../../components/Card"
+import ChekIngrediente from "../../components/CheckIngrediente"
+import Acordeon from '../../components/Acordeon'
 
 function Pedido() {
   const context = useContext(MyContext)
@@ -14,13 +17,32 @@ function Pedido() {
           {context=>{
             const {pizzas}=context.state
             if(pizzas)
-              return(<div className="Pedido">
-              <p>Pedido</p>
-                {context.state.pizzas.map(pizza=>
-              <p>{pizza.nombre}</p>
-                  
-              )}
-             </div>)
+              return(<div className="Pedido" style={{
+                justifyContent:"center",
+                paddingTop:"38vh",
+                position:"relative",
+                flexDirection:"column"
+                
+}}>
+  
+
+  <Grid templateColumns="repeat(2, 1fr)" gap={2}>
+  {context.state.pizzas.map(pizza=>
+    <>
+    <Card 
+    nombre={pizza.nombre}
+    ingredientes={pizza.ingredientes}
+    precio={pizza.precio}
+    img={pizza.image_URL}/>
+    <Acordeon title="Añadir Ingredientes">
+    <ChekIngrediente/>
+    </Acordeon>
+    
+    </>
+    )}
+  </Grid>
+  
+</div>)
              else
                   return <p>Loading</p>
           }}  
